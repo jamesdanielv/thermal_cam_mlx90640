32a6a12343922c3517aedfa1cabeae131b0b0f2b
james <jamesdanielv@msn.com>
2019-08-07 23:11:10 -0700

    Update MLX90640_SimpleDrivers.h

--- Updates/Example1_BasicReadings/MLX90640_SimpleDrivers.h
+++ Updates/Example1_BasicReadings/MLX90640_SimpleDrivers.h
@@ -1,10 +1,90 @@
+//this is data removed area 
+/*we are going to simplify ram usage, then simplify math when on arduino.
+*for example we want less ram usage up front,
+*so
+        float alpha[768]; //768x4bytes   3072bytes
+        int16_t offset[768];768x2        1536bytes             
+        float kta[768];  //768x4bytes    3072bytes
+        float kv[768];//768x4bytes       3072bytes
+        uint16_t eeMLX90640[832];        1664bytes
+        are going to not need to be calculated on the fly.
+        so ~12416 bytes less upfront! (not including other ram stuff
 
-uint8_t fast_2_to_powerof0_15( uint8_t x){
-//since we know pow of 2^ is only 16 values we simplify
-float value;//this stores value
-switch (x){
-  case 0: value=1;break;case 1: value=2;break;case 2: value=4;break;case 3: value=8;break;case 4: value=16;break;case 5: value=32;break;case 6: value=64;break;case 7: value=128;break;case 8: value=256;break;
-  case 9: value=512;break;case 10: value=1024;break;case 11: value=2048;break;case 12: value=4096;break;case 13: value=8192;break;case 14: value=16384;break;case 15: value=32768;break;
+        1st build 19164 bytes
+
+
+*/
+//first data redundancy is offset paramiters. we will get them raw
+//------------------------------------------------------------------------------
+/*
+void ExtractOffsetParameters(uint16_t *eeData, paramsMLX90640 *mlx90640)
+{
+    int occRow[24];
+    int occColumn[32];
+    int p = 0;
+    int16_t offsetRef;
+    uint8_t occRowScale;
+    uint8_t occColumnScale;
+    uint8_t occRemScale;
+    
+
+    occRemScale = (eeData[16] & 0x000F);
+    occColumnScale = (eeData[16] & 0x00F0) >> 4;
+    occRowScale = (eeData[16] & 0x0F00) >> 8;
+    offsetRef = eeData[17];
+    if (offsetRef > 32767)
+    {
+        offsetRef = offsetRef - 65536;
+    }
+    
+    for(int i = 0; i < 6; i++)
+    {
+        p = i * 4;
+        occRow[p + 0] = (eeData[18 + i] & 0x000F);
+        occRow[p + 1] = (eeData[18 + i] & 0x00F0) >> 4;
+        occRow[p + 2] = (eeData[18 + i] & 0x0F00) >> 8;
+        occRow[p + 3] = (eeData[18 + i] & 0xF000) >> 12;
+    }
+    
+    for(int i = 0; i < 24; i++)
+    {
+        if (occRow[i] > 7)
+        {
+            occRow[i] = occRow[i] - 16;
+        }
+    }
+    
+    for(int i = 0; i < 8; i++)
+    {
+        p = i * 4;
+        occColumn[p + 0] = (eeData[24 + i] & 0x000F);
+        occColumn[p + 1] = (eeData[24 + i] & 0x00F0) >> 4;
+        occColumn[p + 2] = (eeData[24 + i] & 0x0F00) >> 8;
+        occColumn[p + 3] = (eeData[24 + i] & 0xF000) >> 12;
+    }
+    
+    for(int i = 0; i < 32; i ++)
+    {
+        if (occColumn[i] > 7)
+        {
+            occColumn[i] = occColumn[i] - 16;
+        }
+    }
+
+    for(int i = 0; i < 24; i++)
+    {
+        for(int j = 0; j < 32; j ++)
+        {
+            p = 32 * i +j;
+            mlx90640->offset[p] = (eeData[64 + p] & 0xFC00) >> 10;
+            if (mlx90640->offset[p] > 31)
+            {
+                mlx90640->offset[p] = mlx90640->offset[p] - 64;
+            }
+            mlx90640->offset[p] = mlx90640->offset[p]*(1 << occRemScale);
+            mlx90640->offset[p] = (offsetRef + (occRow[i] << occRowScale) + (occColumn[j] << occColumnScale) + mlx90640->offset[p]);
+        }
+    }
 }
-return value;// done quick hopefully.  
-}
+
+*/
