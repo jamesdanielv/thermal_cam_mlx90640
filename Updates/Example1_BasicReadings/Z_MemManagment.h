c9af948a34975c517cc8df6cbafce64a8b155ea6
james <jamesdanielv@msn.com>
2019-08-21 11:20:58 -0700

    Create Z_MemManagment.h

---
+++ Updates/Example1_BasicReadings/Z_MemManagment.h
@@ -0,0 +1,3 @@
+//this just tells us mem method to use
+#define NEW_METHOD true
+#define use_ram_cache true //this uses 128 bytes more of ram, but makes it transfer more efficeint when using new_method of mem managment
