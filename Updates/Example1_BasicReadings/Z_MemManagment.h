cc6f41e24f52bb7e10b96c89fb1af429ff1bdbf2
james <jamesdanielv@msn.com>
2019-08-23 15:49:48 -0700

    ram cache is a setting at this point. it currently is not in active use. it is for caching of ram values from read of i2c bus
    ram cache is a setting at this point. it currently is not in active use. it is for caching of ram values from read of i2c bus
    added comments to indicate it is not functional as of yet.

--- Updates/Example1_BasicReadings/Z_MemManagment.h
+++ Updates/Example1_BasicReadings/Z_MemManagment.h
@@ -1,3 +1,5 @@
 //this just tells us mem method to use
 #define NEW_METHOD true
+//use_ram_cache - this setting is in the works. currently it does nothing
 #define use_ram_cache true //this uses 128 bytes more of ram, but makes it transfer more efficeint when using new_method of mem managment
+
