905284244c61b6268b635d3377b88e6372574dfd
james <jamesdanielv@msn.com>
2019-08-23 15:41:53 -0700

    Update MLX90640_API.cpp

--- Updates/Example1_BasicReadings/MLX90640_API.cpp
+++ Updates/Example1_BasicReadings/MLX90640_API.cpp
@@ -101,7 +101,7 @@ float Q_rsqrt( float number ) //a good enough square root method.
   x2 = number * 0.5F;
   y  = number;
   i  = * ( long * ) &y;                       // evil floating point bit level hacking
-  i  = 0x5f3759df - ( i >> 1 );               // what the fuck? 
+  i  = 0x5f3759df - ( i >> 1 );               // ######edited language###
   y  = * ( float * ) &i;
   y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
 //  y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
