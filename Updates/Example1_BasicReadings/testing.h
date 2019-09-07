323b35d375f173a81aa94594cb942cab2cf1db36
james <jamesdanielv@msn.com>
2019-09-07 11:38:59 -0700

    this is for timing and other troubleshooting

---
+++ Updates/Example1_BasicReadings/testing.h
@@ -0,0 +1,8 @@
+//this has some additonal values for testing output performance and output values
+
+
+float alphaScale_testing;
+float kvScale_testing;
+float ktaScale1_testing;
+
+float gainEE_testing;
